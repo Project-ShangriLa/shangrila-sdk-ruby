3b0fe5e3a7b8c5873d515261190eddabae8c6a2d
AKB428 <otoraru@gmail.com>
2015-06-01 23:49:59 +0900

    fix doc

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 #ShangriLa SDK for Ruby
 
-
 ##Install
 
 ``gem install shangrila``
@@ -8,3 +7,29 @@
 or Gemfile
 
 ``gem 'shangrila'``
+
+##Usage
+
+### Get master record
+
+```Shangrila::Sora.new().get_master_data(year, cours)```
+
+```Shangrila::Sora.new().get_master_data(2014, 1)```
+
+### Get anime title list
+
+```Shangrila::Sora.new().get_title_list(year, cours)```
+
+```Shangrila::Sora.new().get_title_list(2015, 1)```
+
+### Get anime title map
+
+```Shangrila::Sora.new().get_map_with_key_title(year, cours, property)```
+
+```Shangrila::Sora.new().get_map_with_key_title(2015, 2, 'twitter_account')```
+
+### Get flat data
+
+```Shangrila::Sora.new().get_flat_data(year, cours, property_list)```
+
+```Shangrila::Sora.new().get_flat_data(2014, 2, ['title', 'twitter_hash_tag'])```
--- lib/shangrila/sora.rb
+++ lib/shangrila/sora.rb
@@ -13,17 +13,26 @@ module Shangrila
     def initialize
     end
 
-    #Shangrila::Sora.new().get_title_list(year, cours)
+    # @param [Int] year データ取得対象のアニメの年
+    # @param [Int] cours データ取得対象のアニメの年のクール番号 1-4
+    # @return [JSON] アニメのマスターデータすべて
     def get_master_data(year, cours)
       response = HTTPClient.get(sprintf("%s/%s/%s", URL, year, cours))
       JSON.load(response.body)
     end
 
+    # @param [Int] year データ取得対象のアニメの年
+    # @param [Int] cours データ取得対象のアニメの年のクール番号 1-4
+    # @return [JSON] アニメのマスターデータのタイトルリスト
     def get_title_list(year, cours)
       master_list = get_master_data(year, cours)
       master_list.map{|master| master['title']}
     end
 
+    # @param [Int] year データ取得対象のアニメの年
+    # @param [Int] cours データ取得対象のアニメの年のクール番号 1-4
+    # @param [property] データ取得対象のプロパティ
+    # @return [HASH] タイトルをkey、propertyに対応する値をValueとしたハッシュ
     def get_map_with_key_title(year, cours, property)
       master_list = get_master_data(year, cours)
 
@@ -34,7 +43,10 @@ module Shangrila
       master_map
     end
 
-    ## Shangrila::Sora.new().get_flat_data(year, cours, ['title', 'twitter_hash_tag'])
+    # @param [Int] year データ取得対象のアニメの年
+    # @param [Int] cours データ取得対象のアニメの年のクール番号 1-4
+    # @param [property] データ取得対象のプロパティリスト
+    # @return [Array] 1タイトル1配列としたフラット配列
     def get_flat_data(year, cours, property_list)
       master_list = get_master_data(year, cours)
       records = master_list.map{|master| property_list.map{|p|master[p]} }
